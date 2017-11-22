aae9eb4fe074487f796b8af432a69cdcdf05c855
Sean Lerner <sean@smallcity.ca>
2017-11-22 16:00:02 -0500

    Ties event create and update to city attr rather than location association

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,7 +16,8 @@ class EventsController < ApplicationController
     @event.name = params[:event][:name]
     @event.date = params[:event][:date]
     @event.capacity = params[:event][:capacity]
-
+    @event.location = params[:event][:city]
+    
     if @event.save
       redirect_to event_url(@event)
     else
@@ -33,7 +34,7 @@ class EventsController < ApplicationController
     @event.name = params[:event][:name]
     @event.date = params[:event][:date]
     @event.capacity = params[:event][:capacity]
-    @event.location = @location
+    @event.location = params[:event][:city]
 
     if @event.save
       redirect_to events_url
